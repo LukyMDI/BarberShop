2ba740e2d28bfc05c7293f34679623de9df76efb
LukyMDI <97931181+LukyMDI@users.noreply.github.com>
2022-09-05 04:48:50 -0300

    0.0.2

---
+++ Javascript/script.js
@@ -0,0 +1,22 @@
+var ver_mais = document.getElementById('ver-mais');
+var ver_menos = document.getElementById('ver-menos');
+var quad_oculto = document.getElementsByClassName('quad-oculto');
+var bg_2 = document.getElementById('bg-2');
+
+ver_mais.addEventListener('click', function(){
+    quad_oculto[0].style.display = "flex";
+    quad_oculto[1].style.display = "flex";
+    quad_oculto[2].style.display = "flex";
+    quad_oculto[3].style.display = "flex";
+    ver_mais.style.display = "none";
+    ver_menos.style.display = "block";
+});
+
+ver_menos.addEventListener('click', function(){
+    quad_oculto[0].style.display = "none";
+    quad_oculto[1].style.display = "none";
+    quad_oculto[2].style.display = "none";
+    quad_oculto[3].style.display = "none";
+    ver_menos.style.display = "none";
+    ver_mais.style.display = "block";
+})
